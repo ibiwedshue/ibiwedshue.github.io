7a74b077e11cefb0ae88076143b2f69bd83f5424
get2hue <get2hue@gmail.com>
2015-12-16 18:44:33 -0600

    countdown

--- js/script.js
+++ js/script.js
@@ -112,7 +112,7 @@
 
 	
 	/*CountDown*/
-	$('.married_coundown').countdown({until: new Date(2015, 12, 19)});
+	$('.married_coundown').countdown({until: new Date(2016, 02, 13)});
 	
 	/*OWL Carousel in Our Story*/
 	$(".story_wrapper").owlCarousel({
