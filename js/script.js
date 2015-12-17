916d8c8aba09f34348a45f88645db8d38ba0f65f
get2hue <get2hue@gmail.com>
2015-12-16 18:54:08 -0600

    a

--- js/script.js
+++ js/script.js
@@ -112,7 +112,7 @@
 
 	
 	/*CountDown*/
-	$('.married_coundown').countdown({until: new Date(2016, 12-4, 13)});
+	$('.married_coundown').countdown({until: new Date(2016, 13, 2)});
 	
 	/*OWL Carousel in Our Story*/
 	$(".story_wrapper").owlCarousel({
